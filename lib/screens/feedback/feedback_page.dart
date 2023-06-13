45eaab90dc1d254acbb15f319347150086294119
yoann <yoan@iMac-de-Yoann.local>
2023-06-13 12:29:11 +0200

    [+] Added all page content, no back for the moment [+]

--- lib/screens/feedback/feedback_page.dart
+++ lib/screens/feedback/feedback_page.dart
@@ -1,7 +1,14 @@
 import 'package:molkky_match/utils/imports.dart';
 
-class FeedbackPage extends StatelessWidget {
+class FeedbackPage extends StatefulWidget {
   const FeedbackPage({super.key});
+
+  @override
+  State<FeedbackPage> createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends State<FeedbackPage> {
+  TextEditingController textarea = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +17,97 @@ class FeedbackPage extends StatelessWidget {
 
     return AppScaffold(
         scaffoldKey: scaffoldKey,
-        hauteur: 120,
-        body: const Text("Feedback")
+        hauteur: 30,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Qu'en penses-tu ?",
+              style: AppStyles.pageTitleStyle,
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "Nous vous remercions d’utiliser notre application et nous serions ravis de pouvoir l’améliorer. Alors n’hésitez-pas à nous faire un retour !",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "Noter votre expérience :",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 30),
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FaIcon(
+                  FontAwesomeIcons.faceAngry,
+                  size: 46,
+                  color: AppColors.whiteColor,
+                ),
+                FaIcon(
+                  FontAwesomeIcons.faceGrimace,
+                  size: 46,
+                  color: AppColors.whiteColor,
+                ),
+                FaIcon(
+                  FontAwesomeIcons.faceMeh,
+                  size: 46,
+                  color: AppColors.whiteColor,
+                ),
+                FaIcon(
+                  FontAwesomeIcons.faceSmileBeam,
+                  size: 46,
+                  color: AppColors.whiteColor,
+                ),
+                FaIcon(
+                  FontAwesomeIcons.faceGrinHearts,
+                  size: 46,
+                  color: AppColors.whiteColor,
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "Laissez un commentaire :",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: textarea,
+              keyboardType: TextInputType.multiline,
+              maxLines: 4,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: HexColor("E6E6E6").withOpacity(0.85),
+                border: const OutlineInputBorder(
+                    borderSide: BorderSide(width: 1, color: Colors.red),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(width: 1, color: AppColors.primaryColor),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              style: AppStyles.primaryButtonStyle,
+              child: const Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  'Envoyer',
+                  style: AppStyles.primaryButtonTextStyle,
+                ),
+              ),
+            ),
+          ],
+        )
     );
   }
 }
